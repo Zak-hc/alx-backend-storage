019c16793304ff630e6039e6681e2b04e7c1f6b0
Zak-hc <zhaci001@gmail.com>
2024-03-27 20:05:17 -0700

    task task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 import redis
 import uuid
 from typing import Union, Callable
+import functools
 
 
 class Cache:
@@ -35,9 +36,23 @@ class Cache:
     def clear(self):
         """Effacer l'intégralité du cache."""
         self._redis.flushdb()
-
+    
+    @functools.wraps(set)
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store input data in Redis and return the randomly generated key."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+def count_calls(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        self_instance = args[0]  # 'self' is the instance of the Cache class
+        method_name = func.__qualname__  # Qualified name of the method
+        count_key = f"{method_name}_calls"  # Key for counting method calls
+        self_instance._redis.incr(count_key)  # Increment count for method calls
+        return func(*args, **kwargs)
+    return wrapper
+
+# Decorate the 'store' method of Cache class with 'count_calls'
+Cache.store = count_calls(Cache.store)
