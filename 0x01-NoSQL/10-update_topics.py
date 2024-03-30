c2db289767f08fef5ce54486b73e7f12246964aa
Zak-hc <zhaci001@gmail.com>
2024-03-30 02:52:19 +0000

    updating_all_coll_py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Changes all topics of a school document based on the name """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ Update Topics Function """
+    try:
+        mongo_collection.update_many(
+            {"name": name},
+            {"$set": {"topics": topics}}
+        )
+    except Exception as ex:
+        print(str(ex))
